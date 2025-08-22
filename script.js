7a7e979438af6cb36ff34b4b3261e3a560340c2e
LeonOAO <kkwan812@gmail.com>
2025-08-22 10:16:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -258,8 +258,8 @@ async function fetchExternalContent(url, content, tabIndex) {
 
     const proxies = [
         `https://api.cors.lol/?url=${encodeURIComponent(url)}`,
-        `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`,
-        `https://api.codetabs.com/v1/tmp/?quest=${encodeURIComponent(url)}`,
+      //  `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`,
+      //  `https://api.codetabs.com/v1/tmp/?quest=${encodeURIComponent(url)}`,
         `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`
         /*,
                 `https://corsproxy.io/?url=${encodeURIComponent(url)}`*/
